52597ebf12ba9ef0b20cca1ea573f6c2c3efdd49
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-07-09 23:55:05 +0900

    chore: Fixture monkey 의존성 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter:1.0.0")
+    testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.0.20")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
