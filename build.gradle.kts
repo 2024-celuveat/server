f2cf94ab8636933a9eb2c6da243b77b406ff9677
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-07-12 21:15:19 +0900

    chore: kotest, mockk 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,18 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    // Ko-test
+    testImplementation("io.kotest:kotest-runner-junit5:5.4.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.4.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+
+    // Mockk
+    testImplementation("io.mockk:mockk:1.13.11")
+
+    // Fixture Monkey
     testImplementation("com.navercorp.fixturemonkey:fixture-monkey-starter-kotlin:1.0.20")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
