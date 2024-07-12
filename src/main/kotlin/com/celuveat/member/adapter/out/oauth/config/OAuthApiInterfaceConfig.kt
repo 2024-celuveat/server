1deb96447654f5827a56db3650dd608858e0ff68
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-07-12 14:55:02 +0900

    refactor: HttpInterfaceClient 구현체 변경 -> RestClient

--- src/main/kotlin/com/celuveat/member/adapter/out/oauth/config/OAuthApiInterfaceConfig.kt
+++ src/main/kotlin/com/celuveat/member/adapter/out/oauth/config/OAuthApiInterfaceConfig.kt
@@ -1,15 +1,19 @@
 package com.celuveat.member.adapter.out.oauth.config
 
 import com.celuveat.member.adapter.out.oauth.kakao.KakaoApiClient
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.support.WebClientAdapter
+import org.springframework.http.HttpStatusCode
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.support.RestClientAdapter
 import org.springframework.web.service.invoker.HttpServiceProxyFactory
 
 
 @Configuration
-class OAuthApiClientInterfaceConfig {
+class OAuthApiClientInterfaceConfig(
+) {
+    private val log = LoggerFactory.getLogger(this.javaClass)!!
 
     @Bean
     fun kakaoApiClient(): KakaoApiClient {
@@ -17,8 +21,27 @@ class OAuthApiClientInterfaceConfig {
     }
 
     private fun <T> createHttpInterface(clazz: Class<T>): T {
+        val restClient = buildRestClientWithStatusHandler()
         return HttpServiceProxyFactory.builderFor(
-            WebClientAdapter.create(WebClient.create())
+            RestClientAdapter.create(restClient)
         ).build().createClient(clazz)
+    }
+
+    private fun buildRestClientWithStatusHandler(): RestClient {
+        val restClient = RestClient.builder()
+            .defaultStatusHandler(
+                { status: HttpStatusCode -> status.is4xxClientError },
+                { _, response ->
+                    log.error("Client Error Code={}", response.statusCode)
+                    log.error("Client Error Message={}", String(response.body.readAllBytes()))
+                })
+            .defaultStatusHandler(
+                { status: HttpStatusCode -> status.is5xxServerError },
+                { _, response ->
+                    log.error("Server Error Code={}", response.statusCode)
+                    log.error("Server Error Message={}", String(response.body.readAllBytes()))
+                })
+            .build()
+        return restClient
     }
 }
