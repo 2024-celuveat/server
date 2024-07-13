3349d50a2374a5d8df330c7ee369bfa10ae1d188
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-07-14 00:12:13 +0900

    refactor: 예외 클래스 네이밍 수정

--- src/main/kotlin/com/celuveat/auth/exception/JwtExceptions.kt
+++ src/main/kotlin/com/celuveat/auth/exception/JwtExceptions.kt
@@ -3,15 +3,15 @@ package com.celuveat.auth.exception
 import com.celuveat.common.exception.CeluveatException
 import org.springframework.http.HttpStatus
 
-sealed class JwtExceptions(
+sealed class JwtException(
     status: HttpStatus,
     errorMessage: String,
 ) : CeluveatException(status, errorMessage)
 
-data object InvalidJwtTokenException : JwtExceptions(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다") {
+data object InvalidJwtTokenException : JwtException(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다") {
     private fun readResolve(): Any = InvalidJwtTokenException
 }
 
 data class NoSuchClaimException(
     val key: String,
-) : JwtExceptions(HttpStatus.BAD_REQUEST, "토큰에 $key 클레임이 없습니다")
+) : JwtException(HttpStatus.BAD_REQUEST, "토큰에 $key 클레임이 없습니다")
