a1263c58f7675d56999fdb7c0ffcd67960e50d8b
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-07-30 23:48:47 +0900

    style: ktlint

--- src/main/kotlin/com/celuveat/celeb/domain/ChannelId.kt
+++ src/main/kotlin/com/celuveat/celeb/domain/ChannelId.kt
@@ -4,9 +4,8 @@ import com.celuveat.celeb.exceptions.InvalidChannelIdException
 import com.celuveat.common.utils.throwWhen
 
 data class ChannelId(
-    val value: String
+    val value: String,
 ) {
-
     init {
         throwWhen(value.isBlank()) { InvalidChannelIdException }
         throwWhen(!value.startsWith("@")) { InvalidChannelIdException }
