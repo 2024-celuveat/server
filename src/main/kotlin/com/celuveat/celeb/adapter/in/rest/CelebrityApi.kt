a9d1bc0de69e8e3950998f4c788e776dd14d6b3e
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-07-31 23:28:44 +0900

    docs: 인증 API 명세 추가

--- src/main/kotlin/com/celuveat/celeb/adapter/in/rest/CelebrityApi.kt
+++ src/main/kotlin/com/celuveat/celeb/adapter/in/rest/CelebrityApi.kt
@@ -3,11 +3,13 @@ package com.celuveat.celeb.adapter.`in`.rest
 import com.celuveat.auth.adaptor.`in`.rest.AuthId
 import com.celuveat.celeb.adapter.`in`.rest.response.CelebrityResponse
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 
 @Tag(name = "셀럽 API")
 interface CelebrityApi {
+    @SecurityRequirement(name = "JWT")
     @Operation(summary = "관심 셀럽 목록 조회")
     @GetMapping("/interested")
     fun getInterestedCelebrities(
