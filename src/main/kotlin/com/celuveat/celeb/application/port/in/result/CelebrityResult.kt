d6bddeeab0fe53fcab0555817f0a2042939e026a
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-08-01 16:22:14 +0900

    feat: 조회 결과 Dto 정의

--- src/main/kotlin/com/celuveat/celeb/application/port/in/result/CelebrityResult.kt
+++ src/main/kotlin/com/celuveat/celeb/application/port/in/result/CelebrityResult.kt
@@ -1,7 +1,6 @@
 package com.celuveat.celeb.application.port.`in`.result
 
 import com.celuveat.celeb.domain.Celebrity
-import com.celuveat.celeb.domain.YoutubeChannel
 
 data class CelebrityResult(
     val id: Long,
@@ -23,26 +22,7 @@ data class CelebrityResult(
     }
 }
 
-data class YoutubeChannelResult(
-    val id: Long,
-    val channelId: String,
-    val channelUrl: String,
-    val channelName: String,
-    val contentsName: String,
-    val restaurantCount: Int,
-    val subscriberCount: Long,
-) {
-    companion object {
-        fun from(youtubeChannel: YoutubeChannel): YoutubeChannelResult {
-            return YoutubeChannelResult(
-                id = youtubeChannel.id,
-                channelId = youtubeChannel.channelId.value,
-                channelUrl = youtubeChannel.channelUrl,
-                channelName = youtubeChannel.channelName,
-                contentsName = youtubeChannel.contentsName,
-                restaurantCount = youtubeChannel.restaurantCount,
-                subscriberCount = youtubeChannel.subscriberCount,
-            )
-        }
-    }
-}
+data class SimpleCelebrityResult(
+    val name: String,
+    val profileImageUrl: String,
+)
