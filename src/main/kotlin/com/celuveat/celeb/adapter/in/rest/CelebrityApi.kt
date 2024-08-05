058fa24faf102280a0d62ebdd4d4e607d26ffb09
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-08-05 15:17:22 +0900

    feat: 관심 셀럽 추가 삭제 API 추가

--- src/main/kotlin/com/celuveat/celeb/adapter/in/rest/CelebrityApi.kt
+++ src/main/kotlin/com/celuveat/celeb/adapter/in/rest/CelebrityApi.kt
@@ -3,9 +3,14 @@ package com.celuveat.celeb.adapter.`in`.rest
 import com.celuveat.auth.adaptor.`in`.rest.AuthId
 import com.celuveat.celeb.adapter.`in`.rest.response.CelebrityResponse
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 
 @Tag(name = "셀럽 API")
 interface CelebrityApi {
@@ -15,4 +20,32 @@ interface CelebrityApi {
     fun getInterestedCelebrities(
         @AuthId memberId: Long,
     ): List<CelebrityResponse>
+
+    @SecurityRequirement(name = "JWT")
+    @Operation(summary = "관심 셀럽 추가")
+    @PostMapping("/interested/{celebrityId}")
+    fun addInterestedCelebrity(
+        @AuthId memberId: Long,
+        @Parameter(
+            `in` = ParameterIn.PATH,
+            description = "셀럽 ID",
+            example = "1",
+            required = true,
+        )
+        @PathVariable celebrityId: Long,
+    )
+
+    @SecurityRequirement(name = "JWT")
+    @Operation(summary = "관심 셀럽 삭제")
+    @DeleteMapping("/interested/{celebrityId}")
+    fun deleteInterestedCelebrity(
+        @AuthId memberId: Long,
+        @Parameter(
+            `in` = ParameterIn.PATH,
+            description = "셀럽 ID",
+            example = "1",
+            required = true,
+        )
+        @PathVariable celebrityId: Long,
+    )
 }
--- src/test/kotlin/com/celuveat/celeb/adapter/in/rest/CelebrityControllerTest.kt
+++ src/test/kotlin/com/celuveat/celeb/adapter/in/rest/CelebrityControllerTest.kt
@@ -1,7 +1,11 @@
 package com.celuveat.celeb.adapter.`in`.rest
 
 import com.celuveat.auth.application.port.`in`.ExtractMemberIdUseCase
+import com.celuveat.celeb.application.port.`in`.AddInterestedCelebrityUseCase
+import com.celuveat.celeb.application.port.`in`.DeleteInterestedCelebrityUseCase
 import com.celuveat.celeb.application.port.`in`.GetInterestedCelebritiesUseCase
+import com.celuveat.celeb.application.port.`in`.command.AddInterestedCelebrityCommand
+import com.celuveat.celeb.application.port.`in`.command.DeleteInterestedCelebrityCommand
 import com.celuveat.celeb.application.port.`in`.result.CelebrityResult
 import com.celuveat.support.sut
 import com.fasterxml.jackson.databind.ObjectMapper
@@ -12,18 +16,22 @@ import io.mockk.every
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.delete
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 
 @WebMvcTest(CelebrityController::class)
 class CelebrityControllerTest(
     @Autowired val mockMvc: MockMvc,
     @Autowired val mapper: ObjectMapper,
     @MockkBean val getInterestedCelebritiesUseCase: GetInterestedCelebritiesUseCase,
+    @MockkBean val addInterestedCelebrityUseCase: AddInterestedCelebrityUseCase,
+    @MockkBean val deleteInterestedCelebrityUseCase: DeleteInterestedCelebrityUseCase,
     // for AuthMemberArgumentResolver
     @MockkBean val extractMemberIdUseCase: ExtractMemberIdUseCase,
 ) : FunSpec({
 
-    context("관심 목록의 셀럽을 조회 한다") {
+    context("관심 셀럽의 목록을 조회 한다") {
         val memberId = 1L
         val accessToken = "celuveatAccessToken"
         val results = sut.giveMeBuilder<CelebrityResult>()
@@ -37,6 +45,46 @@ class CelebrityControllerTest(
             }.andExpect {
                 status { isOk() }
                 content { json(mapper.writeValueAsString(results)) }
+            }.andDo {
+                print()
+            }
+        }
+    }
+
+    context("관심 셀럽을 추가 한다") {
+        val memberId = 1L
+        val accessToken = "celuveatAccessToken"
+        val celebrityId = 1L
+        val command = AddInterestedCelebrityCommand(memberId, celebrityId)
+
+        test("추가 성공") {
+            every { extractMemberIdUseCase.extract(accessToken) } returns memberId
+            every { addInterestedCelebrityUseCase.addInterestedCelebrity(command) } returns Unit
+
+            mockMvc.post("/celebrities/interested/{celebrityId}", celebrityId) {
+                header("Authorization", "Bearer $accessToken")
+            }.andExpect {
+                status { isOk() }
+            }.andDo {
+                print()
+            }
+        }
+    }
+
+    context("관심 셀럽을 삭제 한다") {
+        val memberId = 1L
+        val accessToken = "celuveatAccessToken"
+        val celebrityId = 1L
+        val command = DeleteInterestedCelebrityCommand(memberId, celebrityId)
+
+        test("삭제 성공") {
+            every { extractMemberIdUseCase.extract(accessToken) } returns memberId
+            every { deleteInterestedCelebrityUseCase.deleteInterestedCelebrity(command) } returns Unit
+
+            mockMvc.delete("/celebrities/interested/{celebrityId}", celebrityId) {
+                header("Authorization", "Bearer $accessToken")
+            }.andExpect {
+                status { isOk() }
             }.andDo {
                 print()
             }
