c4c4f719b2a8f457502abbfd11f7a85e7c67315c
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-08-08 20:21:07 +0900

    feat: 인기 셀럽 조회 쿼리 추가

--- src/main/kotlin/com/celuveat/celeb/adapter/out/persistence/CelebrityPersistenceAdapter.kt
+++ src/main/kotlin/com/celuveat/celeb/adapter/out/persistence/CelebrityPersistenceAdapter.kt
@@ -1,19 +1,33 @@
 package com.celuveat.celeb.adapter.out.persistence
 
+import com.celuveat.celeb.adapter.out.persistence.entity.CelebrityJpaRepository
 import com.celuveat.celeb.adapter.out.persistence.entity.CelebrityPersistenceMapper
 import com.celuveat.celeb.adapter.out.persistence.entity.CelebrityYoutubeContentJpaRepository
+import com.celuveat.celeb.adapter.out.persistence.entity.InterestedCelebrityJpaRepository
 import com.celuveat.celeb.adapter.out.persistence.entity.RestaurantInVideoJpaRepository
 import com.celuveat.celeb.adapter.out.persistence.entity.YoutubeContentJpaEntity
 import com.celuveat.celeb.application.port.out.FindCelebritiesPort
 import com.celuveat.celeb.domain.Celebrity
 import com.celuveat.common.annotation.Adapter
+import com.celuveat.member.adapter.out.persistence.entity.MemberJpaRepository
 
 @Adapter
 class CelebrityPersistenceAdapter(
+    private val celebrityJpaRepository: CelebrityJpaRepository,
     private val celebrityYoutubeContentJpaRepository: CelebrityYoutubeContentJpaRepository,
+    private val interestedCelebrityJpaRepository: InterestedCelebrityJpaRepository,
     private val restaurantInVideoJpaRepository: RestaurantInVideoJpaRepository,
+    private val memberJpaRepository: MemberJpaRepository,
     private val celebrityPersistenceMapper: CelebrityPersistenceMapper,
 ) : FindCelebritiesPort {
+    override fun findInterestedCelebrities(memberId: Long): List<Celebrity> {
+        memberJpaRepository.getById(memberId)
+        val celebrities = interestedCelebrityJpaRepository.findAllCelebritiesByMemberId(memberId)
+        val celebrityIds = celebrities.map { it.id }
+        val youtubeContentsByCelebrity = celebritiesToContentMap(celebrityIds)
+        return celebrities.map { celebrityPersistenceMapper.toDomain(it, youtubeContentsByCelebrity[it.id]!!) }
+    }
+
     override fun findVisitedCelebritiesByRestaurants(restaurantIds: List<Long>): Map<Long, List<Celebrity>> {
         val celebritiesWithRestaurant = restaurantInVideoJpaRepository.findVisitedCelebrities(restaurantIds)
         val celebrityIds = celebritiesWithRestaurant.map { it.celebrity.id }
@@ -33,4 +47,10 @@ class CelebrityPersistenceAdapter(
         celebrityYoutubeContentJpaRepository.findByCelebrityIdIn(celebrityIds)
             .groupBy { it.celebrity.id }
             .mapValues { (_, celebrityYoutubeContents) -> celebrityYoutubeContents.map { it.youtubeContent } }
+
+    override fun findBestCelebrities(): List<Celebrity> {
+        return celebrityJpaRepository.findAllBySubscriberCountDesc().map {
+            celebrityPersistenceMapper.toDomainWithoutYoutubeContent(it)
+        }
+    }
 }
--- src/main/kotlin/com/celuveat/celeb/adapter/out/persistence/entity/CelebrityJpaRepository.kt
+++ src/main/kotlin/com/celuveat/celeb/adapter/out/persistence/entity/CelebrityJpaRepository.kt
@@ -3,9 +3,21 @@ package com.celuveat.celeb.adapter.out.persistence.entity
 import com.celuveat.celeb.exceptions.NotFoundCelebrityException
 import com.celuveat.common.utils.findByIdOrThrow
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface CelebrityJpaRepository : JpaRepository<CelebrityJpaEntity, Long> {
     override fun getById(id: Long): CelebrityJpaEntity {
         return this.findByIdOrThrow(id) { NotFoundCelebrityException }
     }
+
+    @Query(
+        """
+            SELECT DISTINCT (c), yc.subscriberCount
+            FROM CelebrityYoutubeContentJpaEntity cyc
+            JOIN cyc.youtubeContent yc
+            JOIN cyc.celebrity c
+            ORDER BY yc.subscriberCount DESC LIMIT 15
+        """
+    )
+    fun findAllBySubscriberCountDesc(): Set<CelebrityJpaEntity>
 }
