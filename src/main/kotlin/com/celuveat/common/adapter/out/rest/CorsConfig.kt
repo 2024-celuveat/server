1a38c872507eb48dbb6b49fdc05c78eb382426f8
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-08-08 21:42:41 +0900

    hotfix: Cors 설정 추가

---
+++ src/main/kotlin/com/celuveat/common/adapter/out/rest/CorsConfig.kt
@@ -0,0 +1,31 @@
+package com.celuveat.common.adapter.out.rest
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.http.HttpMethod.DELETE
+import org.springframework.http.HttpMethod.GET
+import org.springframework.http.HttpMethod.OPTIONS
+import org.springframework.http.HttpMethod.PATCH
+import org.springframework.http.HttpMethod.POST
+import org.springframework.http.HttpMethod.PUT
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class CorsConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods(
+                OPTIONS.name(),
+                GET.name(),
+                POST.name(),
+                PUT.name(),
+                PATCH.name(),
+                DELETE.name(),
+            )
+            .allowCredentials(true)
+            .exposedHeaders(AUTHORIZATION, "*")
+    }
+}
