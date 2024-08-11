4c6cab2ed7ade4774cec197d4442ebc5bfc6d6c8
신동훈 <huipulci1@naver.com>
2024-08-11 21:05:09 +0900

    feat: add unique constrains in HelpfulReviewJpaEntity

--- src/main/kotlin/com/celuveat/review/adapter/out/persistence/entity/HelpfulReviewJpaEntity.kt
+++ src/main/kotlin/com/celuveat/review/adapter/out/persistence/entity/HelpfulReviewJpaEntity.kt
@@ -11,8 +11,11 @@ import jakarta.persistence.GenerationType.IDENTITY
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import jakarta.persistence.UniqueConstraint
 
 @Entity
+@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["review_id", "member_id"])])
 class HelpfulReviewJpaEntity(
     @Id
     @GeneratedValue(strategy = IDENTITY)
