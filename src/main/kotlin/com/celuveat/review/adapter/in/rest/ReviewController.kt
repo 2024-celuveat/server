85001e25bf37d38b39659c22d8109db96f7bfc9a
신동훈 <huipulci1@naver.com>
2024-08-11 12:57:37 +0900

    chore: add todo

--- src/main/kotlin/com/celuveat/review/adapter/in/rest/ReviewController.kt
+++ src/main/kotlin/com/celuveat/review/adapter/in/rest/ReviewController.kt
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+// TODO test
 @RequestMapping("/reviews")
 @RestController
 class ReviewController(
