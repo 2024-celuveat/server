46808a1990dc4e7cf59b3ebc30e32004fa78f6d0
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-08-27 13:35:22 +0900

    fix: 조회 음식점 존재 여부 검증 추가

--- src/main/kotlin/com/celuveat/celeb/adapter/out/persistence/VideoPersistenceAdapter.kt
+++ src/main/kotlin/com/celuveat/celeb/adapter/out/persistence/VideoPersistenceAdapter.kt
@@ -5,14 +5,19 @@ import com.celuveat.celeb.adapter.out.persistence.entity.VideoPersistenceMapper
 import com.celuveat.celeb.application.port.out.ReadVideoPort
 import com.celuveat.celeb.domain.Video
 import com.celuveat.common.annotation.Adapter
+import com.celuveat.common.utils.throwWhen
+import com.celuveat.restaurant.adapter.out.persistence.entity.RestaurantJpaRepository
+import com.celuveat.restaurant.exception.NotFoundRestaurantException
 
 @Adapter
 class VideoPersistenceAdapter(
     private val videoJpaRepository: VideoJpaRepository,
     private val videoPersistenceMapper: VideoPersistenceMapper,
+    private val restaurantJpaRepository: RestaurantJpaRepository,
 ) : ReadVideoPort {
 
     override fun readVideosInRestaurant(restaurantId: Long): List<Video> {
+        throwWhen(!restaurantJpaRepository.existsById(restaurantId)) { NotFoundRestaurantException }
         val videos = videoJpaRepository.findByRestaurantId(restaurantId)
         return videos.map { videoPersistenceMapper.toDomain(it) }
     }
