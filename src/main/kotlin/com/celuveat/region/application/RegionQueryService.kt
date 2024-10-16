64283ce3f9390f083e473bc7d6bca9fcc27a4cd0
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-10-16 18:24:34 +0900

    feat: 대표 지역 조회 로직 추가

---
+++ src/main/kotlin/com/celuveat/region/application/RegionQueryService.kt
@@ -0,0 +1,16 @@
+package com.celuveat.region.application
+
+import com.celuveat.region.application.port.`in`.ReadRepresentativeRegionsUseCase
+import com.celuveat.region.application.port.`in`.result.RepresentativeRegionResult
+import com.celuveat.region.application.port.out.ReadRegionPort
+import org.springframework.stereotype.Service
+
+@Service
+class RegionQueryService(
+    private val readRegionPort: ReadRegionPort,
+) : ReadRepresentativeRegionsUseCase {
+
+    override fun readRepresentativeRegions(): List<RepresentativeRegionResult> {
+        return readRegionPort.readRepresentativeRegions()
+    }
+}
--- src/main/kotlin/com/celuveat/region/application/port/out/ReadRegionPort.kt
+++ src/main/kotlin/com/celuveat/region/application/port/out/ReadRegionPort.kt
@@ -1,7 +1,9 @@
 package com.celuveat.region.application.port.out
 
+import com.celuveat.region.application.port.`in`.result.RepresentativeRegionResult
 import com.celuveat.region.domain.Region
 
 interface ReadRegionPort {
     fun readByName(name: String): List<Region>
+    fun readRepresentativeRegions(): List<RepresentativeRegionResult>
 }
