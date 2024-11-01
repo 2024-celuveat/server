f64d6c67a043888a4ef12533560f12d6314a040a
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-11-01 16:51:04 +0900

    hotfix: 대표 지역 응답에 위경도 정보 포함

--- src/main/kotlin/com/celuveat/region/adapter/out/static/RegionStaticRepository.kt
+++ src/main/kotlin/com/celuveat/region/adapter/out/static/RegionStaticRepository.kt
@@ -9,13 +9,48 @@ class RegionStaticRepository(
     property: CloudFrontProperty,
 ) {
     private val representedRegions: List<RepresentativeRegionResult> = listOf(
-        RepresentativeRegionResult("잠실", "${property.domain}/regions/jamsil.webp"),
-        RepresentativeRegionResult("성수", "${property.domain}/regions/seongsu.webp"),
-        RepresentativeRegionResult("홍대", "${property.domain}/regions/hongdae.webp"),
-        RepresentativeRegionResult("을지로", "${property.domain}/regions/euljiro.webp"),
-        RepresentativeRegionResult("압구정", "${property.domain}/regions/apgujeong.webp"),
-        RepresentativeRegionResult("여의도", "${property.domain}/regions/yeouido.webp"),
-        RepresentativeRegionResult("이태원", "${property.domain}/regions/leetaewon.webp"),
+        RepresentativeRegionResult(
+            name = "잠실",
+            imageUrl = "${property.domain}/regions/jamsil.webp",
+            latitude = 37.5067945,
+            longitude = 127.0830482
+        ),
+        RepresentativeRegionResult(
+            name = "성수",
+            imageUrl = "${property.domain}/regions/seongsu.webp",
+            latitude = 37.5436099,
+            longitude = 127.0428194
+        ),
+        RepresentativeRegionResult(
+            name = "홍대",
+            imageUrl = "${property.domain}/regions/hongdae.webp",
+            latitude = 37.5507254,
+            longitude = 126.9256382
+        ),
+        RepresentativeRegionResult(
+            name = "을지로",
+            imageUrl = "${property.domain}/regions/euljiro.webp",
+            latitude = 37.5660286,
+            longitude = 126.9954924
+        ),
+        RepresentativeRegionResult(
+            name = "압구정",
+            imageUrl = "${property.domain}/regions/apgujeong.webp",
+            latitude = 37.5271478,
+            longitude = 127.0334517
+        ),
+        RepresentativeRegionResult(
+            name = "여의도",
+            imageUrl = "${property.domain}/regions/yeouido.webp",
+            latitude = 37.5295808,
+            longitude = 126.9326803
+        ),
+        RepresentativeRegionResult(
+            name = "이태원",
+            imageUrl = "${property.domain}/regions/leetaewon.webp",
+            latitude = 37.5385051,
+            longitude = 126.9925224
+        ),
     )
 
     fun readRepresentativeRegions(): List<RepresentativeRegionResult> {
--- src/main/kotlin/com/celuveat/region/application/port/in/result/RepresentativeRegionResult.kt
+++ src/main/kotlin/com/celuveat/region/application/port/in/result/RepresentativeRegionResult.kt
@@ -3,4 +3,6 @@ package com.celuveat.region.application.port.`in`.result
 data class RepresentativeRegionResult(
     val name: String,
     val imageUrl: String,
+    val latitude: Double,
+    val longitude: Double,
 )
