15b17c22527f660cc1bf59f883cacf93104b05fa
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2024-11-01 17:02:39 +0900

    hotfix: 대표 지역 응답에 위경도 정보 포함

--- src/main/kotlin/com/celuveat/region/adapter/in/rest/response/RepresentativeRegionResponse.kt
+++ src/main/kotlin/com/celuveat/region/adapter/in/rest/response/RepresentativeRegionResponse.kt
@@ -5,12 +5,16 @@ import com.celuveat.region.application.port.`in`.result.RepresentativeRegionResu
 data class RepresentativeRegionResponse(
     val name: String,
     val imageUrl: String,
+    val latitude: Double,
+    val longitude: Double,
 ) {
     companion object {
         fun from(result: RepresentativeRegionResult): RepresentativeRegionResponse {
             return RepresentativeRegionResponse(
                 name = result.name,
                 imageUrl = result.imageUrl,
+                latitude = result.latitude,
+                longitude = result.longitude,
             )
         }
     }
