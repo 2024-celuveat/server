b5e14b5d90279105baecf8bd476a144d28414a21
신동훈 <huipulci1@naver.com>
2024-12-08 13:27:47 +0900

    V4 add sourcelink in restaurant image

--- src/main/kotlin/com/celuveat/restaurant/domain/RestaurantImage.kt
+++ src/main/kotlin/com/celuveat/restaurant/domain/RestaurantImage.kt
@@ -4,6 +4,7 @@ data class RestaurantImage(
     val id: Long = 0,
     val name: String,
     val author: String,
+    val sourceLink: String?,
     val url: String,
     val isThumbnail: Boolean,
 )
